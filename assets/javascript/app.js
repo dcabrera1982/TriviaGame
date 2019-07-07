6d650f1e6b4df64ed75b03aabdb3161864925e6f
David Cabrera <dcabrera1982@gmail.com>
2019-07-07 17:37:37 -0400

    first three questions added

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -5,19 +5,55 @@
 //correct answer will be shown to the player along with a picture related to the correct answer.
 //page will automatically reload after 5 seconds to the next question without user input.
 
-var score = 0;
-var question = [
-    {prompt:  "Which player once scored 100 points in a single NBA game?\n(a) Michael Jordan\n\ (b) Kobe Bryant\n\ (c) Wilt Chamberlain\n\ (d) Kareem Abdul Jabbar",
-    answer: "c"
+
+function timedQuiz(){}
+
+
+function finalResults(){}
+
+
+timedQuiz();
+
+
+submitButton.addEventListener('click', finalResults);
+
+
+
+
+
+var gameQuestion = [
+    {
+     question:  "Which player once scored 100 points in a single NBA game?",
+     answers: {
+         a: Michael Jordan, 
+         b: Kobe Bryant, 
+         c: Wilt Chamberlain,
+         d: Kareem Abdul Jabbar
+    },
+         correctAnswer: "c"
 },
-    {prompt:  "Everyone knows about Dr. J, but do you know his real name?\n(a) James Harden\n\ (b) Julius Erving\n\ (c) Julius Randle\n\ (d) John Stockton",
-    answer: "b"
+    {
+        question:  "Everyone knows about Dr. J, but do you know his real name?",
+        answers: {
+            a: James Harden, 
+            b: Julius Erving, 
+            c: Julius Randle, 
+            d: John Stockton
+    },
+        correctAnswer: "b"
 },
-    {prompt:  "I hold the record for most career points with 38,387, who am I?\n(a) Karl Malone\n\ (b) Michael Jordan\n\ (c) Lebron James\n\ (d) Kareem Abdul Jabbar",
-    answer: "d"
-},
+    {
+        question:  "I hold the record for most career points with 38,387, who am I?",
+        answers: {
+            a: Karl Malone, 
+            b: Michael Jordan, 
+            c: Lebron James, 
+            d: Kareem Abdul Jabbar
+        },
+        correctAnswer: "d"
+}
     // {prompt:  }
-]
+];
 
 for (var i=0; i < question.length; i++){
     var answer = window.prompt(question[i].prompt);
