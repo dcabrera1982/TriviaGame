6f26b07b4d791bf427ef3cc35987c3741c168e60
David Cabrera <dcabrera1982@gmail.com>
2019-07-11 20:46:22 -0400

    added function for countdown timer

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,6 +1,6 @@
 // Initial values
 
-var counter = 30;
+var counter = 5;
 var currentQuestion = 0;
 var score = 0;
 var loss = 0;
@@ -8,12 +8,18 @@ var timer;
 
 // Start 30 second timer for the user to respond or choose an answer to each question
 
+function countDown() {
+    counter--;
 
+    $("#time").html('Timer: ' + counter);
+}
 
 
 // Display the question and the choices together in the browser
 
 function loadQuestion() {
+    counter = 5;
+    timer = setInterval(countDown, 1000);
 
     var question = gameQuestions[currentQuestion].question;
     var answers = gameQuestions[currentQuestion].answers;
