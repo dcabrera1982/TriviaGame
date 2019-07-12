046fc79943b6dc004b0c891a5cbd2f46810f565f
David Cabrera <dcabrera1982@gmail.com>
2019-07-11 20:32:15 -0400

    question and answer functions added

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,67 +1,32 @@
-//Upon screen refresh a random multiple choice questions appears.
-//player has 30 seconds to guess answer.
-//player will choose a,b,c or d.
-//If player does not choose answer within 15 seconds they automatically get answer "wrong".
-//correct answer will be shown to the player along with a picture related to the correct answer.
-//page will automatically reload after 5 seconds to the next question without user input.
+// Initial values
+
+var counter = 30;
+var currentQuestion = 0;
+var score = 0;
+var loss = 0;
+var timer;
 
 
-function timedQuiz(){}
+// Display the question and the choices together in the browser
 
+function loadQuestion() {
 
-function finalResults(){}
+    var question = gameQuestions[currentQuestion].question;
+    var answers = gameQuestions[currentQuestion].answers;
 
+    $('#time').html('Timer: ' + counter);
+    $('#game').html('<h4>' + question + '</h4>');
 
-timedQuiz();
-
-
-submitButton.addEventListener('click', finalResults);
-
-
-
-
-
-var gameQuestion = [
-    {
-     question:  "Which player once scored 100 points in a single NBA game?",
-     answers: {
-         a: Michael Jordan, 
-         b: Kobe Bryant, 
-         c: Wilt Chamberlain,
-         d: Kareem Abdul Jabbar
-    },
-         correctAnswer: "c"
-},
-    {
-        question:  "Everyone knows about Dr. J, but do you know his real name?",
-        answers: {
-            a: James Harden, 
-            b: Julius Erving, 
-            c: Julius Randle, 
-            d: John Stockton
-    },
-        correctAnswer: "b"
-},
-    {
-        question:  "I hold the record for most career points with 38,387, who am I?",
-        answers: {
-            a: Karl Malone, 
-            b: Michael Jordan, 
-            c: Lebron James, 
-            d: Kareem Abdul Jabbar
-        },
-        correctAnswer: "d"
 }
-    // {prompt:  }
-];
 
-for (var i=0; i < question.length; i++){
-    var answer = window.prompt(question[i].prompt);
-    if(response == question[i].answer){
-        score++;
-        alert("That's right!");
-    }   else {
-            alert("WRONG!");
-        }
+function loadAnswers(answers) {
+    let result = '';
+
+    for (var i = 0; i < answers.length; i++) {
+        result += `<p class="answers" data-answer="${answers[i]}">${answers[i]}</p>`;
     }
-alert("you got " + score + "/" + question.length);
+
+    return result;
+}
+
+loadQuestion();
---
+++ assets/javascript/questions.js
@@ -0,0 +1,32 @@
+var gameQuestions = [
+        {
+            question: "Which player once scored 100 points in a single NBA game?",
+            answers: [
+                "Michael Jordan",
+                "Kobe Bryant",
+                "Wilt Chamberlain",
+                "Kareem Abdul Jabbar"
+            ],
+            correctAnswer: "Wilt Chamberlain"
+        },
+        {
+            question: "Everyone knows about Dr. J, but do you know his real name?",
+            answers: [
+                "James Harden",
+                "Julius Erving",
+                "Julius Randle",
+                "John Stockton"
+            ],
+            correctAnswer: "Julius Erving"
+        },
+        {
+            question: "I hold the record for most career points with 38,387, who am I?",
+            answers: [
+                "Karl Malone",
+                "Michael Jordan",
+                "Lebron James",
+                "Kareem Abdul Jabbar"
+            ],
+            correctAnswer: "Kareem Abdul Jabbar"
+        }
+]; 
