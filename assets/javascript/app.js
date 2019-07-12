9c6a705aa323ab60df8b63350fbf3f77f3aaa305
David Cabrera <dcabrera1982@gmail.com>
2019-07-11 21:25:50 -0400

    made reset button functional

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -6,12 +6,42 @@ var score = 0;
 var loss = 0;
 var timer;
 
+
+// Once timer is over, go to the next question
+function nextQuestion() {
+
+    var isQuestionOver = (gameQuestions.length - 1) === currentQuestion;
+    if (isQuestionOver) {
+
+        console.log("Game Over");
+        displayResult();
+    } else {
+    currentQuestion++;
+    loadQuestion();
+}
+
+}
+
+
+
 // Start 30 second timer for the user to respond or choose an answer to each question
+function timeUp() {
+    clearInterval(timer);
+
+    loss++;
+
+    nextQuestion();
+}
+
 
 function countDown() {
     counter--;
 
     $("#time").html('Timer: ' + counter);
+
+    if (counter === 0) {
+        timeUp();
+    }
 }
 
 
@@ -40,5 +70,49 @@ function loadAnswers(answers) {
 
     return result;
 }
+
+
+// Go to the next question when either the correct/wrong answer have been selected
+
+$(document).on("click", ".answers", function() {
+    clearInterval(timer);
+    var selectedAnswer = $(this).attr('data-answer');
+    var correctAnswer = gameQuestions[currentQuestion].correctAnswer;
+
+    if (selectedAnswer === correctAnswer) {
+        score++;    
+        nextQuestion();
+        console.log("Winner");    
+    } else {
+        loss++;
+        nextQuestion();
+        console.log("Missed Shot!")
+    }
+
+    console.log("what", selectedAnswer);
+
+});
+
+function displayResult() {
+    var result = `
+        <p>You got ${score} questions(s) right</p>
+        <p>You missed ${loss} question(s)</p>
+        <p>Total questions: ${gameQuestions.length}</p>
+        <button class="btn btn-primary" id="reset">Reset Game</button>
+    `;
+
+    $('#game').html(result);
+}
+
+$(document).on('click', '#reset', function() {
+     counter = 5;
+     currentQuestion = 0;
+     score = 0;
+     loss = 0;
+     timer = null;
+
+     loadQuestion();
+})
+
 
 loadQuestion();
